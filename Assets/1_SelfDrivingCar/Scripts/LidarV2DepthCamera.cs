6bf3825dc8c74e21392fc5beeaaaf8a579f32544
yang wang <yang wang>
2017-07-18 00:38:41 -0700

    add fisheye shader

--- Assets/1_SelfDrivingCar/Scripts/LidarV2DepthCamera.cs
+++ Assets/1_SelfDrivingCar/Scripts/LidarV2DepthCamera.cs
@@ -3,31 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[ExecuteInEditMode]
 public class LidarV2DepthCamera : MonoBehaviour {
 
-	public Cubemap cubemap;
-
+    public Material material;
 	public RawImage ri;
-	// public void Awake()
-	// {
-	// 	GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
-	// 	GetComponent<Camera>().targetTexture.format = RenderTextureFormat.Depth;
-	// }
-	Texture2D tex;
-
-	void Start() {
-		tex = new Texture2D(128, 128);
-		ri.texture = tex;
-
+ 
+	public void Awake() {
+		GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
+		GetComponent<Camera>().targetTexture.format = RenderTextureFormat.Depth;
 	}
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, material);
+    }
 
 	void Update()
 	{
-		GetComponent<Camera>().RenderToCubemap(cubemap);
-
-		Debug.Log(cubemap.texelSize);
-
-		Graphics.CopyTexture(cubemap, 0, 0, 0, 0, 128, 128, tex, 0, 0, 0, 0);
-		tex.Apply();
+		//Graphics.CopyTexture(cubemap, 0, 0, 0, 0, 128, 128, tex, 0, 0, 0, 0);
+		//tex.Apply();
 	}
+}
+
+public class CRTEffect : MonoBehaviour
+{
 }
---
+++ Assets/SphereMapImageEffect.shader
@@ -0,0 +1,31 @@
+﻿Shader "Custom/SphereMapImageEffect" {
+	Properties {
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+		_Fov ("FOV", Float) = 0.333
+	}
+	SubShader {
+		Pass {
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+		
+			uniform sampler2D _MainTex;
+			uniform sampler2D _MaskTex;
+
+			uniform float _Fov;
+ 
+			fixed4 frag (v2f_img i) : COLOR {	
+				i.uv = (i.uv * 2) - 1;
+				float ang = _Fov * 0.5;
+				i.uv.x = tan(ang*i.uv.x) / tan(ang);
+				i.uv.y = tan(ang*i.uv.y) / tan(ang);
+				i.uv = (i.uv * 0.5) + 0.5;
+
+				float4 c = tex2D(_MainTex, i.uv);
+				return c;
+			}
+			ENDCG
+		}
+	}
+}
