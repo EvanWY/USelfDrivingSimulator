c54eb0532797718ad0e5b21399b7d2dadf2c9264
Yang Wang <ywang@tangibleplay.com>
2017-07-20 10:19:00 -0700

    bug fix

--- Assets/1_SelfDrivingCar/Scripts/LidarV2DepthCamera.cs
+++ Assets/1_SelfDrivingCar/Scripts/LidarV2DepthCamera.cs
@@ -7,25 +7,14 @@ using UnityEngine.UI;
 public class LidarV2DepthCamera : MonoBehaviour {
 
     public Material material;
-	public RawImage ri;
  
-	public void Awake() {
-		GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
-		GetComponent<Camera>().targetTexture.format = RenderTextureFormat.Depth;
-	}
+	// public void Awake() {
+	// 	GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
+	// 	GetComponent<Camera>().targetTexture.format = RenderTextureFormat.Depth;
+	// }
 
     void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        Graphics.Blit(source, destination, material);
+        //Graphics.Blit(source, destination, material);
     }
-
-	void Update()
-	{
-		//Graphics.CopyTexture(cubemap, 0, 0, 0, 0, 128, 128, tex, 0, 0, 0, 0);
-		//tex.Apply();
-	}
-}
-
-public class CRTEffect : MonoBehaviour
-{
 }
