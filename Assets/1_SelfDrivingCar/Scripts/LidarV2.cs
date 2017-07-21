343fa51719dd330c9fd5508a3042ee6cf8c2e882
yang wang <yang wang>
2017-07-21 08:28:20 -0700

    change output format of lidar

--- Assets/1_SelfDrivingCar/Scripts/LidarV2.cs
+++ Assets/1_SelfDrivingCar/Scripts/LidarV2.cs
@@ -23,15 +23,13 @@ public class LidarV2 : MonoBehaviour
 	
 	int CloudWidth;
 
-	public Queue<Texture2D> imageQueue = new Queue<Texture2D>();
+	//public Queue<Texture2D> imageQueue = new Queue<Texture2D>();
 	public Texture2D lastImage;
-
 	public Texture2D nextImage;
 
 	public RawImage rawImage;
 
-
-	public Texture2D scaleddd;
+	public Texture2D scaledImage;
 
 	void Start()
 	{
@@ -44,18 +42,13 @@ public class LidarV2 : MonoBehaviour
 
 	public bool TryRenderPointCloud(out byte[] image)
 	{
-		if (imageQueue.Count < 2) {
+		if (lastImage == null) {
 			image = null;
 			return false;
 		}
 
-		while (imageQueue.Count >= 2)
-		{
-			lastImage = imageQueue.Dequeue();
-		}
 		image = lastImage.EncodeToJPG();
-		lastImage.Apply();
-
+		lastImage = null;
 		return true;
 	}
 
@@ -72,7 +65,7 @@ public class LidarV2 : MonoBehaviour
 		
 		while (sampleCount > 0) {
 			nextImage.Apply();
-			imageQueue.Enqueue(nextImage);
+			lastImage = nextImage;
 			nextImage = new Texture2D(CloudWidth, Channels, TextureFormat.RGB24, false);
 			Render(ref nextImage, ref nextStartColumns, ref sampleCount, currCamTheta);
 		}
@@ -97,21 +90,21 @@ public class LidarV2 : MonoBehaviour
 	void ExecuteRender(ref Texture2D targetImage,int renderWidth, ref int imgHorizontalPixelStart, ref int sampleCount, int maxCamRenderWidth, float currCamTheta) {
 		DepthCamera.transform.localEulerAngles = Vector3.up * Mathf.LerpUnclamped(0, 360, (imgHorizontalPixelStart + 0.5f * renderWidth) / (float)(CloudWidth));
 		DepthCamera.Render();
-		Texture2D scaledTex = new Texture2D(DepthCamera.targetTexture.width, DepthCamera.targetTexture.height, TextureFormat.RGB24, false);
+		Texture2D readRenderTex = new Texture2D(DepthCamera.targetTexture.width, DepthCamera.targetTexture.height, TextureFormat.RGB24, false);
 		
 		RenderTexture.active = DepthCamera.targetTexture;
-		scaledTex.ReadPixels(new Rect(0, 0, DepthCamera.targetTexture.width, DepthCamera.targetTexture.height), 0, 0);
-		scaledTex.Apply();
+		readRenderTex.ReadPixels(new Rect(0, 0, DepthCamera.targetTexture.width, DepthCamera.targetTexture.height), 0, 0);
+		readRenderTex.Apply();
 
 		int maxCamRenderHeight = Mathf.RoundToInt(2 * currCamTheta * Channels / (MaximalVerticalFOV - MinimalVerticalFOV));
 		Debug.Log(maxCamRenderWidth + " " + maxCamRenderHeight + " " + currCamTheta);
-		TextureScaler.scale(scaledTex, maxCamRenderWidth, maxCamRenderHeight);
+		//TextureScaler.scale(readRenderTex, maxCamRenderWidth, maxCamRenderHeight);
+		scaledImage = TextureScaler.scaled(readRenderTex, maxCamRenderWidth, maxCamRenderHeight);
+		scaledImage.Apply();
 
 		int srcX = (maxCamRenderWidth - renderWidth) / 2;
 		int srcY = Mathf.RoundToInt(maxCamRenderHeight * (MinimalVerticalFOV + currCamTheta) / (currCamTheta + currCamTheta));
-		Graphics.CopyTexture(scaledTex, 0, 0, srcX, srcY, renderWidth, Channels, targetImage, 0, 0, imgHorizontalPixelStart, 0);
-
-		scaleddd = scaledTex;
+		Graphics.CopyTexture(scaledImage, 0, 0, srcX, srcY, renderWidth, Channels, targetImage, 0, 0, imgHorizontalPixelStart, 0);
 		
 		Debug.Log(srcX + " " + srcY + " " + renderWidth + " " + imgHorizontalPixelStart);
 
